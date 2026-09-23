c0033490bb25ae05347228176d5fd8d68ca8df06
Joecephus2 <nordvpn5000@mail.com>
2026-09-23 18:06:23 -0400

    Fix duplicate MainWindow declaration

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -5,8 +5,6 @@
 #include <QStringList>
 
 class QListWidget;
-
-class QListWidget;
 class QLineEdit;
 
 class MainWindow final : public QMainWindow
@@ -27,24 +25,6 @@ private:
     QStringList videoDirectories;
     QListWidget *videoList = nullptr;
     QLineEdit *searchBox = nullptr;
-};
-
-class MainWindow final : public QMainWindow
-{
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
-
-private:
-    QStringList findMissingDependencies() const;
-
-    void loadConfiguration();
-    void saveConfiguration() const;
-    void scanVideoDirectories();
-    void refreshVideos();
-    void chooseDirectory();
-
-    QStringList videoDirectories;
-    QListWidget *videoList = nullptr;
 };
 
 #endif
